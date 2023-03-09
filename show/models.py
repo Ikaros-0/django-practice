4deab9760e9e73ef68aef647e154d5b421bdee13
Ikaros-0 <2216627957@qq.com>
2023-03-09 14:15:30 +0800

    增加二氧化碳数据模型

--- show/models.py
+++ show/models.py
@@ -9,3 +9,11 @@ class Temperature(models.Model):
 
     def __str__(self) -> str:
         return self.captemperature
+    
+# 二氧化碳数据模型
+class co2(models.Model):
+    captime = models.DateTimeField(auto_now_add=False)
+    capco2 = models.CharField(max_length=10)
+
+    def __str__(self) -> str:
+        return self.capco2
